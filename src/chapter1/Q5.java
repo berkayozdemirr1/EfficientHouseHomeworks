aa6f7631788292bd5956522c35a31f803a1f5c98
berkayozdemirr1 <107854878+berkayozdemirr1@users.noreply.github.com>
2022-10-11 13:29:07 +0300

    replace method changed with replaceFirst method in fifth exercises.

--- src/chapter1/Q5.java
+++ src/chapter1/Q5.java
@@ -4,6 +4,6 @@ public class Q5 {
     public static void main(String[] args) {
         String mesaj = "I hate you. While i walking in every step. I hate you. ";
         System.out.println(mesaj);
-        System.out.println(mesaj.replace("hate", "love"));
+        System.out.println(mesaj.replaceFirst("hate", "love"));
     }
 }
--- src/chapter2/Scanner5.java
+++ src/chapter2/Scanner5.java
@@ -5,6 +5,6 @@ public class Scanner5 {
         java.util.Scanner input = new java.util.Scanner(System.in);
         System.out.print("Please enter your message : ");
         String mesaj = input.nextLine();
-        System.out.println("Changed message is : " + mesaj.replace("hate", "love"));
+        System.out.println("Changed message is : " + mesaj.replaceFirst("hate", "love"));
     }
 }
