3d4ba50b3a66b698b2a14ea93caa9e4a5025dafc
berkayozdemirr1 <107854878+berkayozdemirr1@users.noreply.github.com>
2022-10-28 12:31:52 +0300

    Special Homeworks

---
+++ src/numberUtil/DailyLoop.java
@@ -0,0 +1,16 @@
+package numberUtil;
+
+public class DailyLoop {
+    public static void main(String[] args) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+
+        System.out.print("Malcolm Gladwell'e göre; temel olarak bir konuda uzman olabilmek için toplamda 10 bin saat\n" +
+                "o konu üzerinde çalýþýlmasý gerekmektedir. Sen tahmini kaç saat çalýþtýn :");
+        int success = input.nextInt();
+        while (success++ < 10000) {
+            int remainForSuccess = 10000 - success;
+            System.out.printf("%d saat çalýþmalýsýn.%n", remainForSuccess);
+        }if (success > 10000)
+            System.out.println("Baþarmak compiler kadar hýzlý deðildir. Unutma. Adým adým ilerlemelisin.");
+    }
+}
